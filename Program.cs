e55607f5f247bc7324a216240d37ddb459c8672e
adleyaxel <adleymarchena@gmail.com>
2022-06-08 19:44:21 -0600

    Math

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Suma
+Console.WriteLine(54 + 46);
+
+// Resta
+Console.WriteLine(30 - 16);
+
+// Multiplicación
+Console.WriteLine(12 * 8);
+
+// Division
+Console.WriteLine(Decimal.Divide(24, 5));
+
+// Exponente
+Console.WriteLine(Math.Pow(9, 2));
+
+// Division entera
+Console.WriteLine(50 / 3);
+
+// Modulo
+Console.WriteLine(25 % 3);
